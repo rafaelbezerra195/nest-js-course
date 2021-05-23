c8f74657173bfc0ef51a19264ceb0005127d8a0d
Rafael Bezerra <rafaelbezerra195@gmail.com>
2021-05-23 16:36:16 -0300

    feat(player): create dto and interface/model

--- src/player/interfaces/player.interface.ts
+++ src/player/interfaces/player.interface.ts
@@ -1,1 +1,9 @@
-export interface Player {}
+export interface Player {
+  readonly _id: string;
+  readonly phoneNumber: string;
+  readonly email: string;
+  nome: string;
+  ranking: string;
+  rankingPosition: number;
+  urlPhoto: string;
+}
